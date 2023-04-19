345c50f6b8ef67cb8ba112eb46ea7100c5cf9e68
ZhidieWu <93614558+ZhidieWu@users.noreply.github.com>
2023-04-20 01:15:30 +0200

    add precision score

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 #decision tree
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder
 #
 from mlxtend.feature_selection import SequentialFeatureSelector as SFS
@@ -42,8 +42,14 @@ all_features = df_encoded_input.columns.tolist()
 all_features.remove('activity')
 X = df_encoded_input[all_features]
 y = df_encoded_input['activity']
+x_train,x_test,y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state=42)
 clf = DecisionTreeClassifier(random_state=1234)
-model = clf.fit(X, y)
+model = clf.fit(x_train, y_train)
+#decision_function = clf.decision_function(x_test)
+predict_actual = clf.predict(x_test)
+accuracy_acutal=clf.score(x_test,y_test)
+classification_result = classification_report(y_test,predict_actual)
+print(predict_actual,accuracy_acutal,classification_result,sep='\n')
 rules = get_rules(clf,all_features , ['Connection','Drive','Load','Pause'])
 for r in rules:
     print(r)
