fe73674d7617c60012a910f2d455625280b4b7c0
ZhidieWu <93614558+ZhidieWu@users.noreply.github.com>
2023-06-05 23:27:56 +0200

    validationpart2

--- validationPart2.py
+++ validationPart2.py
@@ -1,12 +1,28 @@
+from pm4py.visualization.bpmn import visualizer as bpmn_visualizer
+from pm4py.visualization.petri_net import visualizer as petri_visualizer
 import pm4py
-import pandas as pd
-bpmn_file = "Data/testPart1.bpmn"
-df = pd.read_csv('Data/new_LHR_AMS.csv', sep=";",parse_dates=True)
+from pm4py.algo.discovery.footprints import algorithm as footprints_discovery
 
-# ## add column name
-df.columns = ["mainShipmentRouteNumber", "speed", "time", "truckNumber", "timestamp", "shipmentEndCity",
-                "shipmentStartCity", "city", "country","activity"]
 
-df.to_csv('Data/dataWithColumn.csv', index=False)
-df = pd.read_csv('Data/dataWithColumn.csv', sep=";",parse_dates=['time'],infer_datetime_format=True)
-dfg,start_activities,end_activities = pm4py.discover_dfg(df,case_id_key="mainShipmentRouteNumber",activity_key="activity",timestamp_key="timestamp")
+
+
+
+bpmn_graph = pm4py.read_bpmn("Data/testPart1.bpmn")
+bpmn_visualization = bpmn_visualizer.apply(bpmn_graph)
+bpmn_visualizer.view(bpmn_visualization)
+net, im, fm = pm4py.convert_to_petri_net(bpmn_graph)
+petri_visualization = petri_visualizer.apply(net)
+bpmn_visualizer.view(petri_visualization)
+# 导出Petri网为PNML文件
+pnml_path = "Data/testPart1.pnml"
+pm4py.write_pnml(net, im, fm, pnml_path)
+log = pm4py.play_out(net, im, fm)
+
+fp_net = footprints_discovery.apply(net, im, fm)
+print(fp_net)
+
+print("###################bpmn##################")
+ft_print = footprints_discovery.apply(bpmn_graph,im,fm)
+nodes,edge=pm4py.objects.bpmn.util.sorting.get_sorted_nodes_edges(bpmn_graph)
+#ft_print = footprints_discovery.trace_by_trace.apply(bpmn_log)
+print(nodes)
